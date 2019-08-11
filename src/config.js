3f13318813a907a32a3d542cec42a5a1adb53970
abhishek-malviya-au1 <abhishekmalviya17@gmail.com>
2019-08-11 15:10:53 +0530

    deploying

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
 
-    consumerKey: '',
-    consumerSecret: '',
-    loginUrl:"",
-    tokenUrl:"",
-    url:""
+    consumerKey: 'N2JZW85NZ1jFj6T2FK0ywc3G1',
+    consumerSecret: 'vU50A0mwKjfl1pmwdDcKMSS7rVZKwWhMuxauKvf6FNatLRKuaK',
+    loginUrl:"https://express-twitter-app.herokuapp.com/auth/twitter/login",
+    tokenUrl:"https://express-twitter-app.herokuapp.com/auth/twitter/request",
+    url:"https://parse-server-for-postit.herokuapp.com"
 };
 
